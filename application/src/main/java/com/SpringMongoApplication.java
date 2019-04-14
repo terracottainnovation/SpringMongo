087143aaeb1d5548dc94c40c0eecd7055dacfdeb
Tejas Mahajan <tejas5mahajan@gmail.com>
2019-04-14 18:56:19 +0530

    adding swagger ui. & api-docs.

--- application/src/main/java/com/SpringMongoApplication.java
+++ application/src/main/java/com/SpringMongoApplication.java
@@ -1,9 +1,10 @@
+package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.terracottainnovation.application.*")
+@ComponentScan("com.terracottainnovation")
 public class SpringMongoApplication {
 
 	public static void main(String[] args) {
