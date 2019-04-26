f4c84ebdd00191d937995c74c560eb1123aa0727
Tejas Mahajan <tejas5mahajan@gmail.com>
2019-04-26 09:10:56 +0530

    moving main class file to src

--- application/src/main/java/SpringMongoApplication.java
+++ application/src/main/java/SpringMongoApplication.java
@@ -1,4 +1,3 @@
-package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
